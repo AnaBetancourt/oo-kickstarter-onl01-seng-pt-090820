f7cd26ceebde200db9252f86ffb1023f47141276
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 04:28:52 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project 
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer 
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+end
